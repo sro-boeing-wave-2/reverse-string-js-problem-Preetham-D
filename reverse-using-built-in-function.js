aac7b79e0c047634c21464f14a74fec4dacca70e
Preetham D <preethamdbangera@gmail.com>
2018-07-11 10:51:35 +0530

    Resolved issues

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,10 +1,4 @@
 // The intent of this file is to s
 // the string using built in functions.
-const reverse = (s) => {
-  const strsplit = s.split('');
-  const strrev = strsplit.reverse();
-  const strjoin = strrev.join('');
-  return (strjoin);
-};
-
+const reverse = s => s.split('').reverse().join('');
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,11 +1,11 @@
 // The intent of this file is to s the string using iterators (for, foreach)
 const reverse = (s) => {
-  let i;
+  // let i;
   let revStr = '';
   // let str = prompt("Please enter the string", "default");
   const strsplit = s.split('');
   const strlen = s.length;
-  for (i = strlen - 1; i >= 0; i -= 1) {
+  for (let i = strlen - 1; i >= 0; i -= 1) {
     revStr += strsplit[i];
   }
   return (revStr);
